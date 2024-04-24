4fcea59aae9356c20d3d031ff28224496f9bd4db
Dahan Schuster <tanbohe@duck.com>
2024-04-24 17:03:01 -0300

    feat: renderiza SelectPerfil só se tiver permissão

--- src/containers/Perfis/SelectPerfil.tsx
+++ src/containers/Perfis/SelectPerfil.tsx
@@ -2,14 +2,18 @@ import { FC } from "react";
 import SelectInput, { SelectInputProps } from "../../components/SelectInput";
 import { usePerfis } from "../../contexts/PerfisContext";
 import useDebounceEffect from "../../hooks/useDebonceEffect";
+import useUserPermissions from "../../hooks/useUserPermissions";
 
 const SelectPerfil: FC<SelectInputProps> = (props) => {
+  const { has } = useUserPermissions();
+  const podeListar = has("perfis-listar");
   const { perfis, getPerfis } = usePerfis();
 
   useDebounceEffect(() => {
-    if (!perfis.length) getPerfis();
+    if (podeListar && !perfis.length) getPerfis();
   }, []);
 
+  if (!podeListar) return null;
   return (
     <SelectInput
       {...props}
