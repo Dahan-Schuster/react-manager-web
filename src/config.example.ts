8a93b5a7686947f1438057e1affacb2ca3e2f5a9
Dahan <tanbohe@duck.com>
2024-04-29 13:04:17 -0300

    feat: chaves do storage no config.example

--- src/config.example.ts
+++ src/config.example.ts
@@ -1,8 +1,15 @@
+const storageBaseName = "projetoPadrao";
+
 const config = {
   appTitle: "Projeto Padrão",
   apiBaseUrl: "http://localhost:3333",
   apiTimeout: 1000 * 60, // 1 min
-  storageKeys: {},
+  storageKeys: {
+    storageKeys: {
+      user: `${storageBaseName}:user`,
+      temaAtivo: `${storageBaseName}:temaAtivo`,
+    },
+  },
 };
 
 export default config;
