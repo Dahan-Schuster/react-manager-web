c34d4bdb35a1ad4fc1d5ff112227fcd63cbdcc66
Dahan <tanbohe@duck.com>
2024-05-06 12:52:56 -0300

    feat: contexto de listagem de temas MUI

---
+++ src/contexts/TemasMuiContext.tsx
@@ -0,0 +1,58 @@
+import {
+  FC,
+  ReactNode,
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+import useAxios from "../services/useAxios";
+
+interface TemasMuiContextValue {
+  temas: Mui.Theme[];
+  setTemas: (x: Mui.Theme[]) => void;
+  getTemas: () => Promise<void>;
+}
+
+const TemasMuiContext = createContext<TemasMuiContextValue | null>(null);
+export default TemasMuiContext;
+
+/**
+ * Contexto para a tela de administração de temas MUI
+ */
+export const TemasMuiProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [temas, setTemas] = useState<Mui.Theme[]>([]);
+  const { makeRequest } = useAxios();
+
+  const getTemas = useCallback(async () => {
+    const response = await makeRequest({
+      url: "/sistema/temas-mui",
+    });
+
+    if (response.success) {
+      setTemas(response.temas as Mui.Theme[]);
+    }
+  }, []);
+
+  return (
+    <TemasMuiContext.Provider
+      value={{
+        temas,
+        setTemas,
+        getTemas,
+      }}
+    >
+      {children}
+    </TemasMuiContext.Provider>
+  );
+};
+
+export const useTemasMui = () => {
+  const context = useContext(TemasMuiContext);
+
+  if (!context) {
+    throw new Error("useTemasMui must be used within an TemasMuiProvider");
+  }
+
+  return context;
+};
