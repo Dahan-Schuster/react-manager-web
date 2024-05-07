1d5d0b6885ce81556a0c3de34c49d99cf6671f59
Dahan <tanbohe@duck.com>
2024-05-07 11:59:21 -0300

    feat: usa formik no painel de edição de tema

--- src/containers/TemasMui/PanelEditarTema.tsx
+++ src/containers/TemasMui/PanelEditarTema.tsx
@@ -1,15 +1,15 @@
 import { ThemeProvider } from "@emotion/react";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 import Paper from "@mui/material/Paper";
 import TextField, { TextFieldProps } from "@mui/material/TextField";
 import { createTheme, darken, lighten } from "@mui/material/styles";
-import { Form, Formik } from "formik";
-import { Dispatch, FC, SetStateAction, useCallback } from "react";
-import SwitchModoTema from "./SwitchModoTema";
-import ContainerAlterarCores from "./ContainerAlterarCores";
+import { Dispatch, FC, SetStateAction, useCallback, useState } from "react";
 import { MuiDefaultPalette, MuiPaletteNames } from "../../constants";
-import Button from "@mui/material/Button";
+import ContainerAlterarCores from "./ContainerAlterarCores";
+import SwitchModoTema from "./SwitchModoTema";
+import { Form, Formik } from "formik";
 
 const inputsMargin = "normal";
 
@@ -22,7 +22,7 @@ const commonTextFieldProps: Partial<TextFieldProps> = {
 interface PanelEditarTemaProps {
   tema: Mui.Theme;
   setTema: Dispatch<SetStateAction<Mui.Theme>>;
-  handleSubmit: () => void;
+  handleSubmit: (values: Mui.Theme) => void;
 }
 
 // tema específico do painel de edição, para diferenciar do tema do sistema e do tema sendo editado
@@ -68,108 +68,141 @@ const PanelEditarTema: FC<PanelEditarTemaProps> = ({
           overflowY: "scroll",
         }}
       >
-        <Box
-          component={"form"}
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit();
-          }}
-        >
-          <TextField
-            label="Nome do tema"
-            {...commonTextFieldProps}
-            onChange={(e) =>
-              setTema((prev) => ({ ...prev, nome: e.target.value }))
-            }
-          />
-          <Divider />
-          <SwitchModoTema tema={tema} setTema={setTema} />
-          <Divider />
-          <ContainerAlterarCores
-            label="Background"
-            cores={[
-              {
-                label: "Padrão",
-                cor: tema.background_default || "#f0f0f0",
-                onChange: (v) =>
-                  setTema((prev) => ({ ...prev, background_default: v })),
-              },
-              {
-                label: "Claro",
-                cor: tema.background_paper || "#ffffff",
-                onChange: (v) =>
-                  setTema((prev) => ({ ...prev, background_paper: v })),
-              },
-            ]}
-          />
-          <Divider />
-          <ContainerAlterarCores
-            label="Texto"
-            cores={[
-              {
-                label: "Primário",
-                cor: tema.text_primary || "#000000",
-                onChange: (v) =>
-                  setTema((prev) => ({ ...prev, text_primary: v })),
-              },
-              {
-                label: "Secundário",
-                cor: tema.text_secondary || "#333333",
-                onChange: (v) =>
-                  setTema((prev) => ({ ...prev, text_secondary: v })),
-              },
-              {
-                label: "Inativo",
-                cor: tema.text_disabled || "#666666",
-                onChange: (v) =>
-                  setTema((prev) => ({ ...prev, text_disabled: v })),
-              },
-            ]}
-          />
-          <Divider />
-          {Object.entries(MuiDefaultPalette).map(([nome, paletaPadrao]) => {
-            let _nome: Mui.PaletteOptions = nome as Mui.PaletteOptions;
-
-            const coresPaleta = tema.cores_paleta?.[_nome] || paletaPadrao;
-
+        <Formik initialValues={tema} enableReinitialize onSubmit={handleSubmit}>
+          {({ getFieldProps }) => {
             return (
-              <ContainerAlterarCores
-                key={_nome}
-                label={MuiPaletteNames[_nome]}
-                cores={[
-                  {
-                    label: "Padrão",
-                    cor: coresPaleta.main,
-                    onChange: (v) => handleChangeCorPaleta(v, _nome, "main"),
-                  },
-                  {
-                    label: "Claro",
-                    cor: coresPaleta.light || lighten(coresPaleta.main, 0.2),
-                    onChange: (v) => handleChangeCorPaleta(v, _nome, "light"),
-                  },
-                  {
-                    label: "Escuro",
-                    cor: coresPaleta.dark || darken(coresPaleta.main, 0.2),
-                    onChange: (v) => handleChangeCorPaleta(v, _nome, "dark"),
-                  },
-                  {
-                    label: "Cor do texto",
-                    cor: coresPaleta.contrastText || "#000",
-                    onChange: (v) =>
-                      handleChangeCorPaleta(v, _nome, "contrastText"),
-                  },
-                ]}
-              />
-            );
-          })}
-          <Divider />
+              <Form>
+                <TextField
+                  label="Nome do tema"
+                  {...commonTextFieldProps}
+                  {...getFieldProps("nome")}
+                />
+                <Divider />
+                <SwitchModoTema tema={tema} setTema={setTema} />
+                <Divider />
+                <ContainerAlterarCores
+                  label="Background"
+                  cores={[
+                    {
+                      label: "Fundo",
+                      cor: tema.background_default || "#f0f0f0",
+                      onChange: (v) =>
+                        setTema((prev) => ({ ...prev, background_default: v })),
+                    },
+                    {
+                      label: "Conteúdo",
+                      cor: tema.background_paper || "#ffffff",
+                      onChange: (v) =>
+                        setTema((prev) => ({ ...prev, background_paper: v })),
+                    },
+                  ]}
+                />
+                <Divider />
+                <ContainerAlterarCores
+                  label="Texto"
+                  cores={[
+                    {
+                      label: "Primário",
+                      cor: tema.text_primary || "#000000",
+                      onChange: (v) =>
+                        setTema((prev) => ({ ...prev, text_primary: v })),
+                    },
+                    {
+                      label: "Secundário",
+                      cor: tema.text_secondary || "#333333",
+                      onChange: (v) =>
+                        setTema((prev) => ({ ...prev, text_secondary: v })),
+                    },
+                    {
+                      label: "Inativo",
+                      cor: tema.text_disabled || "#666666",
+                      onChange: (v) =>
+                        setTema((prev) => ({ ...prev, text_disabled: v })),
+                    },
+                  ]}
+                />
+                <Divider />
+                <ContainerAlterarCores
+                  label="Layout"
+                  cores={[
+                    {
+                      label: "Cabeçalho",
+                      cor:
+                        tema.cor_header ||
+                        tema.cores_paleta.primary?.main ||
+                        MuiDefaultPalette.primary.main,
+                      onChange: (v) =>
+                        setTema((prev) => ({ ...prev, cor_header: v })),
+                    },
+                    {
+                      label: "Menu",
+                      cor: tema.cor_menu || "#fff",
+                      onChange: (v) =>
+                        setTema((prev) => ({ ...prev, cor_menu: v })),
+                    },
+                  ]}
+                />
+                <Divider />
+                {Object.entries(MuiDefaultPalette).map(
+                  ([nome, paletaPadrao]) => {
+                    let _nome: Mui.PaletteOptions = nome as Mui.PaletteOptions;
 
-          <Box p={1}>
-            <Button fullWidth type="submit" variant="contained" color="success">
-              Salvar
-            </Button>
-          </Box>
-        </Box>
+                    const coresPaleta =
+                      tema.cores_paleta?.[_nome] || paletaPadrao;
+
+                    return (
+                      <ContainerAlterarCores
+                        key={_nome}
+                        label={MuiPaletteNames[_nome]}
+                        cores={[
+                          {
+                            label: "Padrão",
+                            cor: coresPaleta.main,
+                            onChange: (v) =>
+                              handleChangeCorPaleta(v, _nome, "main"),
+                          },
+                          {
+                            label: "Claro",
+                            cor:
+                              coresPaleta.light ||
+                              lighten(coresPaleta.main, 0.2),
+                            onChange: (v) =>
+                              handleChangeCorPaleta(v, _nome, "light"),
+                          },
+                          {
+                            label: "Escuro",
+                            cor:
+                              coresPaleta.dark || darken(coresPaleta.main, 0.2),
+                            onChange: (v) =>
+                              handleChangeCorPaleta(v, _nome, "dark"),
+                          },
+                          {
+                            label: "Cor do texto",
+                            cor: coresPaleta.contrastText || "#000",
+                            onChange: (v) =>
+                              handleChangeCorPaleta(v, _nome, "contrastText"),
+                          },
+                        ]}
+                      />
+                    );
+                  }
+                )}
+                <Divider />
+
+                <Box p={1}>
+                  <Button
+                    fullWidth
+                    type="submit"
+                    variant="contained"
+                    color="success"
+                  >
+                    Salvar
+                  </Button>
+                </Box>
+              </Form>
+            );
+          }}
+        </Formik>
       </Box>
     </ThemeProvider>
   );
