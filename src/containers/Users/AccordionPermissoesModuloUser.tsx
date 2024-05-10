11ad153a7ace6bd20cdfbc4a8b377f7c7fcb09dd
Dahan <tanbohe@duck.com>
2024-05-10 11:25:12 -0300

    feat: checkbox de permissão específica do módulo

--- src/containers/Users/AccordionPermissoesModuloUser.tsx
+++ src/containers/Users/AccordionPermissoesModuloUser.tsx
@@ -1,5 +1,9 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 import { Dispatch, FC, SetStateAction } from "react";
 import SimpleAccordion from "../Common/SimpleAccordion";
+import CheckboxPermissaoModulo from "./CheckboxPermissaoModulo";
 import CheckboxTodasPermissoesModulo from "./CheckboxTodasPermissoesModulo";
 
 interface AccordionPermissoesModuloUserProps {
@@ -27,10 +31,32 @@ const AccordionPermissoesModuloUser: FC<AccordionPermissoesModuloUserProps> = ({
         />,
       ]}
     >
-      {modulo.tiposPermissoes.map((tipoPermissao) => (
-        // TODO: Checkboxes de cada permissão do módulo
-        <>{tipoPermissao.label}</>
-      ))}
+      <Grid container spacing={1}>
+        {modulo.tiposPermissoes.map((permModulo) => (
+          <Grid item xs={12} key={permModulo.id}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                "&:hover": {
+                  backgroundColor: (theme) => theme.palette.action.hover,
+                },
+              }}
+            >
+              <Typography flex={1} px={1}>
+                {permModulo.label}
+              </Typography>
+              <CheckboxPermissaoModulo
+                permissao={permModulo}
+                user={user}
+                setUser={setUser}
+              />
+              <Box sx={{ width: 24 }} />
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
     </SimpleAccordion>
   );
 };
---
+++ src/containers/Users/CheckboxPermissaoModulo.tsx
@@ -0,0 +1,66 @@
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import CircularProgress from "@mui/material/CircularProgress";
+import {
+  Dispatch,
+  FC,
+  SetStateAction,
+  memo,
+  useCallback,
+  useState,
+} from "react";
+import { useUsers } from "../../contexts/UsersContext";
+
+interface CheckboxPermissaoModuloProps {
+  permissao: Sistema.PermissaoType;
+  user: Users.UserType;
+  setUser: Dispatch<SetStateAction<Users.UserType>>;
+}
+
+/**
+ * Checkbox que indica se o user tem todas ou alguma permissão para o módulo
+ */
+const CheckboxPermissaoModulo: FC<CheckboxPermissaoModuloProps> = memo(
+  ({ permissao, user, setUser }) => {
+    const possuiPermissao = Boolean(
+      user.permissoes?.find((permPerfil) => permPerfil.id === permissao.id)
+    );
+
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const { updatePermissoes } = useUsers();
+
+    /** Envia requisição de alteração das permissões do user, adicionando ou removendo a permissão atual */
+    const handleChange = useCallback(() => {
+      setLoading(true);
+      updatePermissoes(user.id, {
+        novasPermissoes: possuiPermissao ? [] : [permissao.id],
+        permissoesDeletar: possuiPermissao ? [permissao.id] : [],
+      })
+        .then((res) => res.success && setUser(res.user))
+        .finally(() => setLoading(false));
+    }, [possuiPermissao]);
+
+    if (loading)
+      return (
+        <Box
+          sx={{
+            display: "flex",
+            p: "13px",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress size="1rem" />
+        </Box>
+      );
+    return (
+      <Checkbox
+        checked={possuiPermissao}
+        onChange={handleChange}
+        onClick={(event) => event.stopPropagation()}
+      />
+    );
+  }
+);
+
+export default CheckboxPermissaoModulo;
